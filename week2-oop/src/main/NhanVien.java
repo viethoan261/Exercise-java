d211cb6c672c27f513edbab766b1cb64e22a8dc2
kothichdua12 <lb26012001@gmail.com>
2021-12-15 18:31:18 +0700

    week2
    exercise_week2_OOP

---
+++ week2-oop/src/main/NhanVien.java
@@ -0,0 +1,21 @@
+package main;
+
+public class NhanVien {
+	public String maNV;
+	public String tenNV;
+	public String cmnd;
+	public String email;
+	public String sdt;
+	public Boolean gioTinh;
+	public void tinhLuong()
+	{
+		
+	};
+	public void tongGioLam()
+	{
+		
+	};
+	public void tongNgayNghi() {
+		
+	};
+}
